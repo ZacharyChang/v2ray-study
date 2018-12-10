ed7a12b1868fca6df5ee75901328474508d3895f
Zachary <zacharychang.zc@gmail.com>
2018-12-11 01:30:06 +0800

    update: add auto fmt to errorgen

--- main/errorgen/main.go
+++ main/errorgen/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 )
 
@@ -16,13 +17,16 @@ func getCurrentPkg() (string, error) {
 }
 
 func main() {
+
+	filename := "errors.generated.go"
+
 	pkg, err := getCurrentPkg()
 	if err != nil {
 		log.Fatal("Failed to get current package: ", err.Error())
 		return
 	}
 
-	file, err := os.OpenFile("errors.generated.go", os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0644)
+	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0644)
 	if err != nil {
 		log.Fatalf("Failed to generate errors.generated.go: %v", err)
 		return
@@ -34,6 +38,8 @@ func main() {
 	fmt.Fprintln(file, "")
 	fmt.Fprintln(file, "type errPathObjHolder struct {}")
 	fmt.Fprintln(file, "func newError(values ...interface{}) *errors.Error { return errors.New(values...).WithPathObj(errPathObjHolder{}) }")
-
 	file.Close()
+
+	res, err := exec.Command("go", "fmt", filename).Output()
+	fmt.Println("result:", string(res))
 }
